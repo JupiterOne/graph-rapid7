014d0529ab47fbaf2c94a58ae463f3819f1f1b07
Juan Manuel <juanmanuel@salesbricks.com>
2023-03-22 14:39:06 -0300

    Now we only log the body of the root api route response

--- src/client.ts
+++ src/client.ts
@@ -101,7 +101,8 @@ export class APIClient {
     const rootApiRoute = `https://${this.insightHost}/api/3`;
     try {
       const response = await this.request(rootApiRoute, 'GET');
-      this.logger.info({ response }, 'Root API response');
+      const { body } = await response.json();
+      this.logger.info({ body }, 'Root API response');
     } catch (err) {
       let errMessage = `Error occurred validating invocation at ${rootApiRoute} (code=${err.code}, message=${err.message})`;
       if (err.code === 'DEPTH_ZERO_SELF_SIGNED_CERT') {
