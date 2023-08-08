22b1a968f31a2131ff41f18ee34f79a019aeb631
Ronald Arias <ronald.arias@contractor.jupiterone.com>
2023-08-08 14:57:41 -0500

    fix tests

--- src/steps/site-users.test.ts
+++ src/steps/site-users.test.ts
@@ -6,22 +6,19 @@ import {
 } from '@jupiterone/integration-sdk-testing';
 import { setupRapid7Recording } from '../../test/helpers/recording';
 
-let recording: Recording;
-
-afterEach(async () => {
-  if (recording) {
-    await recording.stop();
-  }
-});
-
 describe('fetch-site-users', () => {
+  let recording: Recording;
+
+  afterEach(async () => {
+    if (recording) {
+      await recording.stop();
+    }
+  });
+
   test('success', async () => {
     recording = setupRapid7Recording({
       name: 'fetch-site-users',
       directory: __dirname,
-      options: {
-        recordFailedRequests: false,
-      },
     });
 
     const stepConfig = buildStepTestConfig(steps.FETCH_SITE_USERS);
--- src/steps/vulnerabilities.test.ts
+++ src/steps/vulnerabilities.test.ts
@@ -6,22 +6,19 @@ import {
 } from '@jupiterone/integration-sdk-testing';
 import { setupRapid7Recording } from '../../test/helpers/recording';
 
-let recording: Recording;
-
-afterEach(async () => {
-  if (recording) {
-    await recording.stop();
-  }
-});
-
 describe('fetch-asset-vulnerabilities', () => {
+  let recording: Recording;
+
+  afterEach(async () => {
+    if (recording) {
+      await recording.stop();
+    }
+  });
+
   test('success', async () => {
     recording = setupRapid7Recording({
       name: 'fetch-asset-vulnerabilities',
       directory: __dirname,
-      options: {
-        recordFailedRequests: false,
-      },
     });
 
     const stepConfig = buildStepTestConfig(steps.FETCH_ASSET_VULNERABILITIES);
